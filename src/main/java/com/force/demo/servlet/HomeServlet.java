bf97d6cd9091185ec1d5a4136e75c1e9529dbb87
John Simone <jsimone@salesforce.com>
2011-08-11 17:13:51 -0700

    add decoding

--- src/main/java/com/force/demo/servlet/HomeServlet.java
+++ src/main/java/com/force/demo/servlet/HomeServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
 
 public class HomeServlet extends HttpServlet {
 
@@ -19,7 +20,15 @@ public class HomeServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String signed_request = req.getParameter("signed_request");
-	    req.setAttribute("test", signed_request);
+		
+		String[] elements = signed_request.split(".");
+		
+		String encodedSignature = elements[0];
+		String payload = elements[1];
+		
+		String data = String.valueOf(DatatypeConverter.parseBase64Binary(payload.replaceAll("-_", "+/")));
+		
+	    req.setAttribute("test", data);
 	    req.getRequestDispatcher("facebook.jsp").forward(req, resp);
 
 	}
